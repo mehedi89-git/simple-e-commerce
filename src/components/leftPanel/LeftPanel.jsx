83880427568ac67edb4acc7d1b2bf9c546694a80
Mehedi Hasan <mehediiu23@gmail.com>
2024-10-11 14:56:06 +0600

    add to cart product

--- src/components/leftPanel/LeftPanel.jsx
+++ src/components/leftPanel/LeftPanel.jsx
@@ -1,74 +1,16 @@
 import { ProductGrid } from "../product/ProductGrid";
 import { FilterSection } from "./FilterSection";
+import { Heading } from "./Heading";
 
-export function LeftPanel() {
+export function LeftPanel({ addProductToCart }) {
 
-  const products = [
-    {
-      id: 1,
-      name: 'Gaming Laptop',
-      price: 1500,
-      image: 'https://via.placeholder.com/150',
-      categories: ['Laptops', 'Gaming'],
-    },
-    {
-      id: 2,
-      name: 'Wireless Mouse',
-      price: 50,
-      image: 'https://via.placeholder.com/150',
-      categories: ['Accessories', 'Peripherals'],
-    },
-    {
-      id: 3,
-      name: 'Mechanical Keyboard',
-      price: 100,
-      image: 'https://via.placeholder.com/150',
-      categories: ['Accessories', 'Peripherals'],
-    },
-    {
-      id: 4,
-      name: 'External Hard Drive',
-      price: 120,
-      image: 'https://via.placeholder.com/150',
-      categories: ['Storage', 'Accessories'],
-    },
-    {
-      id: 5,
-      name: 'Graphics Card',
-      price: 500,
-      image: 'https://via.placeholder.com/150',
-      categories: ['Components', 'Gaming'],
-    },
-    {
-      id: 6,
-      name: 'Portable SSD',
-      price: 200,
-      image: 'https://via.placeholder.com/150',
-      categories: ['Storage', 'Accessories'],
-    },
-    {
-      id: 7,
-      name: 'Gaming Monitor',
-      price: 300,
-      image: 'https://via.placeholder.com/150',
-      categories: ['Monitors', 'Gaming'],
-    },
-    {
-      id: 8,
-      name: 'All-in-One Printer',
-      price: 150,
-      image: 'https://via.placeholder.com/150',
-      categories: ['Peripherals', 'Printers'],
-    },
-  ];
+ 
 
     return (
       <div>
-          <div>
-            <h1>Simple e-commece</h1>
-          </div>
+          <Heading />
           <FilterSection />
-          <ProductGrid products={products} />
+          <ProductGrid addProductToCart={addProductToCart} />
       </div>
     );
   }
--- src/components/product/ProductCart.jsx
+++ src/components/product/ProductCart.jsx
@@ -1,14 +1,19 @@
    import "./ProductCart.css";
-   export const ProductCart = ({ productName, productPrice }) => {
+   export const ProductCart = ({ product, addProductToCart }) => {
+      const handleAddProductToCart = () => {
+        addProductToCart(product)
+      }
+
       return (
-    
         <div className="productCart">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5dyT8FEm-Khi_NBX4-sYdtIUo8EOmxe62eQ&s"
            alt="product-img"
            width="300px" />
-          <p>{productName}</p>
-          <p>{`$${productPrice}`}</p>
-          <button>Add to Cart</button>
+          <p>{product.name}</p>
+          <p>{`$${product.price}`}</p>
+          <button className="b1" onClick={handleAddProductToCart}>
+            Add to Cart
+          </button>
         </div>
     
   )
