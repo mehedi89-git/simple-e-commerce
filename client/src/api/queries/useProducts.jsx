20c5b5d25ee9b517d168391c3bfb04696c29edd1
Mehedi Hasan <mehediiu23@gmail.com>
2024-10-20 06:25:04 +0600

    Move useEffect logice to custom hook

--- client/src/api/queries/useProducts.jsx
+++ client/src/api/queries/useProducts.jsx
@@ -1,7 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import { productServices } from "../services";
 
+
 export const useProducs = () => {
-    const products = productServices.getProducts();
-     
-    return { products };
+    
+    const [products, setProducts] = useState([])
+    useEffect( () => {
+        axios.get('http://localhost:5000/api/products').then((res) =>
+          setProducts(res.data))   
+          .catch((err) => {
+          alert("Failed to load products!");
+          console.error(err)
+        })
+      }, []);
+     return { products };
   }
--- client/src/components/product/ProductGrid.jsx
+++ client/src/components/product/ProductGrid.jsx
@@ -1,28 +1,15 @@
-import axios from 'axios'
 
-import { useEffect, useState } from "react";
+import { useProducs } from '../../api/queries';
 
 import { ProductCart } from "./ProductCart";
 
 import "./ProductGrid.css";
 
+
  
 
 export const ProductGrid = () => {
-  const [products, setProducts] = useState([])
-  useEffect( () => {
-      axios.get('http://localhost:5000/api/products').then((res) =>
-        setProducts(res.data))   
-      .catch((err) => {
-        alert("Failed to load products!");
-        console.error(err)
-      })
-
-   
-      }, []);
- 
-  
-  //  const { products } = useProducts()
+ const { products } = useProducs()
    
   return (
     <div className="productGrid">
